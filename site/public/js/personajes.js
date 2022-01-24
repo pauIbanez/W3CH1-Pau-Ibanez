6005a32de098f85a5326f2e50c600592acd3b20a
Pau Ibáñez <pauibanez2001@gmail.com>
2022-01-24 21:49:47 +0100

    Added inizialize personajes function

--- site/public/js/personajes.js
+++ site/public/js/personajes.js
@@ -3,8 +3,18 @@ import Escudero from "./Escudero.js";
 import Luchador from "./Luchador.js";
 import Rey from "./Rey.js";
 
-const joffrey = new Rey("Joffrey", "Baratheon", 16, 2);
-const jaime = new Luchador("Jaime", "Lannister", 45, "Espada", 7);
-const daenerys = new Luchador("Daenerys", "Targaryen", 30, "Dragones", 10);
-const tyrion = new Asesor("Tyrion", "Lannister", 40, daenerys);
-const bronn = new Escudero("Bronn", "AguasNegras", 54, 0, jaime);
+const inizializePersonajes = () => {
+    const personajes = [];
+
+    const joffrey = new Rey("Joffrey", "Baratheon", 16, 2);
+    const jaime = new Luchador("Jaime", "Lannister", 45, "Espada", 7);
+    const daenerys = new Luchador("Daenerys", "Targaryen", 30, "Dragones", 10);
+    const tyrion = new Asesor("Tyrion", "Lannister", 40, daenerys);
+    const bronn = new Escudero("Bronn", "AguasNegras", 54, 0, jaime);
+
+    personajes.push(joffrey, jaime, daenerys, tyrion, bronn);
+
+    return personajes;
+};
+
+export default inizializePersonajes;
