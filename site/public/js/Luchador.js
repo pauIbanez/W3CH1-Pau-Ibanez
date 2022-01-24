e4d03dfdb92248acfcfecfd330e45d2c1a922163
Pau Ibáñez <pauibanez2001@gmail.com>
2022-01-25 00:15:18 +0100

    Added getImagePath function

--- site/public/js/Luchador.js
+++ site/public/js/Luchador.js
@@ -1,33 +1,34 @@
 import Personaje from "./Personaje.js";
+
 class Luchador extends Personaje {
-  arma;
-  destreza;
+    arma;
+    destreza;
 
-  constructor(
-    nombreLuchador,
-    familiaLuchador,
-    edadLuchador,
-    armaLuchador,
-    valorDestreza
-  ) {
-    super(nombreLuchador, familiaLuchador, edadLuchador);
-    this.arma = armaLuchador;
-    this.destreza = this.filtraDestreza(valorDestreza);
-  }
-
-  filtraDestreza(valorDestreza) {
-    if (valorDestreza < 0) {
-      return 0;
-    } else if (valorDestreza > 10) {
-      return 10;
-    } else {
-      return valorDestreza;
+    constructor(
+        nombreLuchador,
+        familiaLuchador,
+        edadLuchador,
+        armaLuchador,
+        valorDestreza
+    ) {
+        super(nombreLuchador, familiaLuchador, edadLuchador);
+        this.arma = armaLuchador;
+        this.destreza = this.filtraDestreza(valorDestreza);
     }
-  }
 
-  comunicar() {
-    return `${super.comunicar()}Primero pego y luego pregunto`;
-  }
+    filtraDestreza(valorDestreza) {
+        if (valorDestreza < 0) {
+            return 0;
+        }
+        if (valorDestreza > 10) {
+            return 10;
+        }
+        return valorDestreza;
+    }
+
+    comunicar() {
+        return `${super.comunicar()}Primero pego y luego pregunto`;
+    }
 }
 
 export default Luchador;
