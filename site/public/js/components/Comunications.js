5c439a67944473d5b254fff3f4c95dc068a22d60
Pau Ibáñez <pauibanez2001@gmail.com>
2022-01-24 21:59:20 +0100

    Added Comunications class

---
+++ site/public/js/components/Comunications.js
@@ -0,0 +1,19 @@
+class Comunications {
+    element;
+
+    constructor() {
+        this.element = document.createElement("div");
+        this.element.className = "comunications";
+
+        this.generatreHTML();
+    }
+
+    generatreHTML() {
+        this.element.innerHTML = `<p class="comunications__text display-1">Una frase que dice alguien</p>
+      <img
+        class="comunications__picture"
+        src="img/no-one.jpg"
+        alt="Nombre y familia del que habla"
+      />`;
+    }
+}
