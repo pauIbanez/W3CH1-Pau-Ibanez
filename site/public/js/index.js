767dcca7e5c9e1d59e3982c21ef6f21965b4fa71
Pau Ibáñez <pauibanez2001@gmail.com>
2022-01-24 22:04:01 +0100

    Added element inizialization in setup function

--- site/public/js/index.js
+++ site/public/js/index.js
@@ -1,7 +1,9 @@
 import inizializePersonajes from "./personajes.js";
+import Comunications from "./components/Comunications.js";
 
 const setup = () => {
     const personajes = inizializePersonajes();
+    const communicator = new Comunications();
 };
 
 setup();
