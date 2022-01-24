99c66279a4ca3e1bff3b0db48a0ade0dc5206cd4
Pau Ibáñez <pauibanez2001@gmail.com>
2022-01-24 21:46:29 +0100

    Seted up imports for personajes.js

--- site/public/js/personajes.js
+++ site/public/js/personajes.js
@@ -1,3 +1,6 @@
+import Asesor from "./Asesor.js";
+import Escudero from "./Escudero.js";
+import Luchador from "./Luchador.js";
 import Rey from "./Rey.js";
 
 const joffrey = new Rey("Joffrey", "Baratheon", 16, 2);
