f15f926d63abbd1a8ae579910c4947c93a0474ea
Pau Ibáñez <pauibanez2001@gmail.com>
2022-01-25 19:21:39 +0100

    Added comunicator functionality

--- site/public/js/index.js
+++ site/public/js/index.js
@@ -6,17 +6,26 @@ import getCharacterOwnPropperties from "./getCharacterPropperties.js";
 const setup = () => {
     const personajes = inizializePersonajes();
     const cardContainer = document.querySelector(".app.container ul");
-
+    const communicator = new Comunications(document.body, "comunications");
     personajes.forEach((personaje) => {
         const properties = getCharacterOwnPropperties(personaje);
-        const talkButtonAction = () => {
+        const dieButtonAction = () => {
             console.log("button habla");
         };
-        const muereButtonAction = () => {
+        const talkButtonAction = () => {
             console.log("button muere");
+            communicator.element.classList.add("on");
+            communicator.element.querySelector(
+                "img"
+            ).src = `img/${personaje.nombre.toLowerCase()}.jpg`;
+            setTimeout(() => {
+                communicator.element.classList.remove("on");
+            }, 2000);
+            communicator.element.querySelector("p").textContent =
+                personaje.comunicar();
         };
         const talkButton = { text: "habla", action: talkButtonAction };
-        const dieButton = { text: "muere", action: muereButtonAction };
+        const dieButton = { text: "muere", action: dieButtonAction };
         const buttons = [talkButton, dieButton];
         // eslint-disable-next-line no-unused-vars
         const card = new Card(
@@ -28,7 +37,6 @@ const setup = () => {
             buttons
         );
     });
-    const communicator = new Comunications(document.body, "comunications");
 };
 
 setup();
