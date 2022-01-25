8b482039d48588728684d38604b5e1d97cc7f91d
Pau Ibáñez <pauibanez2001@gmail.com>
2022-01-25 18:41:03 +0100

    Added button class

---
+++ site/public/js/components/ButtonComponenet.js
@@ -0,0 +1,20 @@
+import Component from "./Component.js";
+
+class ButtonComponent extends Component {
+    constructor(parentElement, className, text, actions, htmlTag = "div") {
+        super(parentElement, className, htmlTag);
+
+        this.generatreHTML(text);
+        this.addEventListeners(actions);
+    }
+
+    generatreHTML(text) {
+        this.element.textContent = text;
+    }
+
+    addEventListeners(actions) {
+        actions.forEach((action) => {
+            this.element.addEventListener(action.event, action.action);
+        });
+    }
+}
